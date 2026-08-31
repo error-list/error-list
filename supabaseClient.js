bcf75bdc02b55a2ca187a2a2b34c49680923d58c
RossLDN7323 <159752386+RossLDN7323@users.noreply.github.com>
2026-08-31 18:47:18 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,7 +2,7 @@ import { initSupabase } from './api.js';
 
 // TODO: paste your Project URL here (Settings → API → Data API)
 // It looks like: https://xxxxxxxxxxxx.supabase.co
-const SUPABASE_URL = 'PASTE_YOUR_PROJECT_URL_HERE';
+const SUPABASE_URL = 'https://vutjojnsumljuavzwyhw.supabase.co/rest/v1/';
 
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_ZKPFfYPeYw3J4WW6mHZQ3A_1weL-bi4';
 
